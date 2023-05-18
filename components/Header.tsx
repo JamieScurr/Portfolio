ce0e73d279b54c4c897b21c8955afc0619b40f45
Jamie <jamiescurrcs@gmail.com>
2023-05-19 00:40:27 +0100

    Added Logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,13 @@ const Header = (props: Props) => {
 	return (
 		<div className="flex flex-row justify-between p-5  sm:text-xl max-w-7xl mx-auto  overflow-hidden">
 			<div>
-				<h1 className="font-bold text-lg sm:text-2xl">JS</h1>
+				<h1 className="font-bold text-lg sm:text-2xl">
+					<img
+						className="absolute top-1 sm:w-14 w-14 sm:top-3"
+						src="images/JS.svg"
+						alt="Logo"
+					/>
+				</h1>
 			</div>
 
 			<div className="flex sm:hidden text-xl" onClick={toggleMenu}>
